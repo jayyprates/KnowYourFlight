31506a95324ff0f7de7eae9da98eae5b4742dd33
jayyprates <joanaprates2@gmail.com>
2024-11-13 18:02:20 -0300

    Implemented fecth API and storaged the response

--- script.js
+++ script.js
@@ -4,15 +4,34 @@ let searchInput = document.getElementById('search-input')
 class Flight {
     constructor(flightId) {
         this.flightId = flightId; 
+        this.statusData = null; 
     }
 
     getDelay() {
         console.log('Searching delays: /flights/.../delays')
     }
 
-    getStatus() {
-        console.log('Searching delays: /flights/.../...')
+    async getStatus() {
+        const apiUrl =`https://api.magicapi.dev/api/v1/aedbx/aerodatabox/flights/Number/${this.flightId}?withAircraftImage=true&withLocation=false`;
+
+        const data = await fetch(apiUrl, {
+            headers: {
+                'x-magicapi-key': 'cm35z0y7q0001l803rkcl3wiu' 
+            }
+        });
+
+        if (data.status == 204) {
+            alert('Flight not found. Try again'); 
+            return;
+        }
+
+        const response = await data.json(); 
+        this.statusData = response.pop(); 
+        
+        console.log(this.statusData)
     }
+
+    
 }
 
 let lastFlight = undefined; 
