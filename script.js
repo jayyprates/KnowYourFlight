399656db483f9455c4ad7e30e2b9924c91dea9eb
jayyprates <joanaprates2@gmail.com>
2024-11-20 18:34:57 -0300

    Created getFlightData and getAirportData methods, finished the flexbox layout

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
 let button = document.getElementById('search-btn')
 let searchInput = document.getElementById('search-input')
+let aircraftImage = document.getElementById('aircraft-img')
+let aircraftName = document.getElementById('aircraft-name')
+let airportName = document.getElementById('airport-name')
 
 class Flight {
     constructor(flightId) {
         this.flightId = flightId; 
-        this.statusData = null; 
     }
 
-    getDelay() {
-        console.log('Searching delays: /flights/.../delays')
-    }
-
-    async getStatus() {
-        const apiUrl =`https://api.magicapi.dev/api/v1/aedbx/aerodatabox/flights/Number/${this.flightId}?withAircraftImage=true&withLocation=false`;
-
-        const data = await fetch(apiUrl, {
+    async getFlightData () {
+        const apiFlightUrl = `https://api.magicapi.dev/api/v1/aedbx/aerodatabox/flights/Number/${this.flightId}?withAircraftImage=true&withLocation=false`
+        
+        const flightData = await fetch (apiFlightUrl, {
             headers: {
                 'x-magicapi-key': 'cm35z0y7q0001l803rkcl3wiu' 
             }
         });
-
-        if (data.status == 204) {
+        if (flightData.status == 204) {
             alert('Flight not found. Try again'); 
             return;
         }
 
-        const response = await data.json(); 
-        this.statusData = response.pop(); 
-        
-        console.log(this.statusData)
+        const response = await flightData.json();
+        return response.pop(); 
     }
 
-    
+    async getAirportData (airportIcao) {
+        const apiAirportUrl = `https://api.magicapi.dev/api/v1/aedbx/aerodatabox/airports/Icao/${airportIcao}?withRunways=false&withTime=false`
+
+        const airportData = await fetch (apiAirportUrl, {
+            headers: {
+                'x-magicapi-key': 'cm35z0y7q0001l803rkcl3wiu' 
+            }
+        }); 
+
+        const response = await airportData.json();
+        return response; 
+    }
 }
 
 let lastFlight = undefined; 
 
-button.addEventListener('click', (e) => {
+button.addEventListener('click', async (e) => {
     const searchInputValue = searchInput.value; 
     if (!searchInputValue) {
         return
     }
 
     lastFlight = new Flight(searchInputValue); 
-    console.log(lastFlight)
 
-    lastFlight.getDelay()
-    lastFlight.getStatus()
+    const flightData = await lastFlight.getFlightData();
+    const airportData = await lastFlight.getAirportData(flightData.arrival.airport.icao);
+    console.log(flightData)
+    console.log('Airport Data', airportData)
+
+    aircraftImage.src = flightData.aircraft.image.url; 
+    aircraftName.innerHTML = flightData.aircraft.model; 
+    airportName.innerHTML = airportData.shortName; 
 })
